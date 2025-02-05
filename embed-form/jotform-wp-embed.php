90d435a25209bc63d8800f9c8d409e473006d0b0
Sean Murphy <sean@iamseanmurphy.com>
2025-02-05 15:18:36 +0000

    1.3.6

--- embed-form/jotform-wp-embed.php
+++ embed-form/jotform-wp-embed.php
@@ -4,7 +4,7 @@
  * Description:       Securely embed online forms in your WordPress website.
  * Requires at least: 5.3
  * Requires PHP:      7.4
- * Version:           1.3.5
+ * Version:           1.3.6
  * Author:            Jotform
  * Author URI:        https://www.jotform.com
  * License:           GNU General Public License v3
